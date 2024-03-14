b35795fc5c9c3d085c80d3c51360a2ef5f6435b2
ArtemGoloviy <58403271+ArtemGoloviy@users.noreply.github.com>
2024-03-14 14:59:56 +0200

    Включаємо навігаційний граф

--- app/src/main/java/ua/edu/lntu/cw_4/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_4/MainActivity.kt
@@ -3,9 +3,7 @@ package ua.edu.lntu.cw_4
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,32 +13,9 @@ import ua.edu.lntu.cw_4.ui.theme.IPZ_CW_4_Holovii_ArtemTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            IPZ_CW_4_Holovii_ArtemTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            IPZ_CW_4_Holovii_ArtemTheme { Nav() }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IPZ_CW_4_Holovii_ArtemTheme {
-        Greeting("Android")
     }
 }
